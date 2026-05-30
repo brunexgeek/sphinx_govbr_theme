169b1e108df60f022cd64d7efdcc0dc1a1791c1c
Bruno Costa <brunexgeek@users.noreply.github.com>
2026-05-30 16:55:51 -0300

    Improve accessibility

--- scss/build.sh
+++ scss/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR=$(cd $(dirname "$0") && pwd)
-docker run -it --rm -v $DIR:/input -v $DIR/../sphinx_govbr_theme/static/css/:/output dart-sass:latest /input/styles.scss /output/styles.css
+docker run -it --rm -v $DIR:/input -v $DIR/../sphinx_govbr_theme/static/css/:/output dart-sass:latest /input/styles.scss /output/styles-min.css
