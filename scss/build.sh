4a60c959f176f8e86fce731d774c2a9b32b93831
Bruno Costa <brunexgeek@users.noreply.github.com>
2026-06-25 08:13:46 -0300

    Fix headings CSS and remove deprecated 'imports'

--- scss/build.sh
+++ scss/build.sh
@@ -8,4 +8,4 @@ if docker ps 2>&1 | grep -q "/var/run/docker.sock"; then
     SUDO_PREFIX="sudo"
 fi
 
-${SUDO_PREFIX} docker run -it --rm -v $DIR:/input -v $DIR/../sphinx_govbr_theme/static/css/:/output dart-sass:latest /input/styles.scss /output/styles-min.css
+${SUDO_PREFIX} docker run -it --rm -v $DIR:/input -v $DIR/../sphinx_govbr_theme/static/css/:/output dart-sass:latest /input/main.scss /output/main-min.css
